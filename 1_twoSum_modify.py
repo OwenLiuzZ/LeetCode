6f4fa104706f0dbcc4b0a86eab0eec87756de4a8
OwenLiu <liusichao131@163.com>
2018-04-12 12:47:52 +0100

    add better solution with problem 1

---
+++ 1_twoSum_modify.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    # It’s easier to understand if we use the dict in the other way:
+    def twoSum(self, nums, target):
+        d = {}
+        for i, n in enumerate(nums):
+            m = target - n
+            if m in d:
+                return [d[m], i]
+            else:
+                d[n] = i
