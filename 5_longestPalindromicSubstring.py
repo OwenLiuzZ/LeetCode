d744dc56adfd135f311271c6a7c256782b0fd567
Unknown <liusichao131@163.com>
2018-04-20 14:26:38 +0100

    add problem5

---
+++ 5_longestPalindromicSubstring.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        longestPalindrome = ''
+        if len(s) % 2 == 1:
+            mid = head = tail = len(s) / 2
+            for i  in range(len(s)):
+
+
+
+        else:
+
+
